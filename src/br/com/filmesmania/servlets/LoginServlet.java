2e97c227715b3f2c9f613044b90be6c95e13d835
Jonas Torquatro <60902980+CipherCrow@users.noreply.github.com>
2022-12-11 21:37:33 -0300

    Finalizando a View de Login

--- src/br/com/filmesmania/servlets/LoginServlet.java
+++ src/br/com/filmesmania/servlets/LoginServlet.java
@@ -14,8 +14,8 @@ import br.com.filmesmania.services.ConverteJson;
 import br.com.filmesmania.services.RealizaRequisicao;
 
 
-@WebServlet("/paginaprincipal")
-public class Bemvindo extends HttpServlet {
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
